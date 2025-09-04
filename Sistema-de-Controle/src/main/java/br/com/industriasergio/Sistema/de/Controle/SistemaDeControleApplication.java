ae8bd098d4cee11f4a8e27c8f8f9bbe72ff06c02
FIC <fic@info.local>
2025-09-04 19:02:39 -0300

    Primeiro commit, api iniciada

---
+++ Sistema-de-Controle/src/main/java/br/com/industriasergio/Sistema/de/Controle/SistemaDeControleApplication.java
@@ -0,0 +1,13 @@
+package br.com.industriasergio.Sistema.de.Controle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeControleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeControleApplication.class, args);
+	}
+
+}
